c6dcf051a3f9dbd09ac4ef43623a1977208d3e8a
Daniil Chepenko <your@email.com>
2020-05-08 18:23:38 +0300

    Added signup field

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+             form.save()
+             #  log the user in
+             return redirect('/posts')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', { 'form': form })
