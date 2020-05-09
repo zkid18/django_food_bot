e75d312299bae987ac4abeb08d3eec4bf14b0fe2
Daniil Chepenko <your@email.com>
2020-05-09 09:49:13 +0300

    Proper redirect after login

--- accounts/views.py
+++ accounts/views.py
@@ -24,7 +24,10 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('/posts')
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('/posts')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', { 'form': form })
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 from posts.models import Post
 
 class PostList(ListView):
     model = Post
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['image', 'description']
+    success_url = '/'
+    login_url = '/accounts/login'
