0cc17de951987367f2ba07c534facb0a65cf88c8
Daniil Chepenko <your@email.com>
2020-06-15 21:39:21 +0300

    Removed comments from view

--- accounts/views.py
+++ accounts/views.py
@@ -39,38 +39,3 @@ class LogoutView(View):
     def post(self, request):
         logout(request)
         return redirect('/posts')
-
-# @csrf_exempt
-# def signup_view(request):
-#     if request.method == 'POST':
-#          form = UserCreationForm(request.POST)
-#          if form.is_valid():
-#              user = form.save()
-#              #  log the user in
-#              login(request, user)
-#              return redirect('/posts')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'accounts/signup.html', { 'form': form })
-
-
-# def login_view(request):
-#     if request.method == "POST":
-#         form = AuthenticationForm(data=request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             login(request, user)
-#             if 'next' in request.POST:
-#                 return redirect(request.POST.get('next'))
-#             else:
-#                 return redirect('/posts')
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, 'accounts/login.html', { 'form': form })
-
-
-# def logout_view(request):
-#     if request.method == 'POST':
-#             logout(request)
-#             return redirect('/posts')
-
